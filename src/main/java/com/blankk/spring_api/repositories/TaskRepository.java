7d3e68fbb1e0bec0d2933be4c49bbc615dd961de
gabrielblankk <gabrielpbbl4nk@gmail.com>
2025-04-17 19:12:26 -0300

    create User and Task repositories

---
+++ src/main/java/com/blankk/spring_api/repositories/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.blankk.spring_api.repositories;
+
+import com.blankk.spring_api.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_Id(Long id);
+}
---
+++ src/main/java/com/blankk/spring_api/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.blankk.spring_api.repositories;
+
+import com.blankk.spring_api.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
