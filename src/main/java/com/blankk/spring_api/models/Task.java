ce77c41f2771422f602c4daaeb89d99f5dcb1ece
gabrielblankk <gabrielpbbl4nk@gmail.com>
2025-04-19 02:05:22 -0300

    add task DTO constructor and change attribute annotations

--- src/main/java/com/blankk/spring_api/models/Task.java
+++ src/main/java/com/blankk/spring_api/models/Task.java
@@ -1,12 +1,12 @@
 package com.blankk.spring_api.models;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.blankk.spring_api.dtos.TaskCreateDTO;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-
-import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -18,14 +18,22 @@ import java.time.LocalDate;
 public class Task {
     public static final String TABLE_NAME = "task";
 
+    public Task (TaskCreateDTO obj) {
+        setDueDate(obj.getDueDate());
+        setDescription(obj.getDescription());
+        setStatus(obj.getStatus());
+        setUser(obj.getUser());
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    @NotBlank
+    @NotNull
     private User user;
 
     @Column(name = "description", nullable = false)
@@ -35,8 +43,8 @@ public class Task {
 
     @Column(name = "due_date", nullable = false)
     @NotBlank
-    @JsonFormat(pattern = "dd-MM-yyyy")
-    private LocalDate dueDate;
+    @Size(min = 10, max = 10)
+    private String dueDate;
 
     @Column(name = "status", nullable = false)
     @NotBlank
